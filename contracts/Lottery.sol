f79bdd5061b644b57405c8712696543c4e3ef10a
Gjergji Konica <gjergjikonica@gmail.com>
2022-01-27 07:25:22 -0500

    Using Enum For Lottery State since we are going to need a calculating state

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -3,20 +3,22 @@ pragma solidity ^0.6.6;
 
 import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol";
 import "@chainlink/contracts/src/v0.6/vendor/SafeMathChainlink.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Lottery {
+contract Lottery is Ownable {
     using SafeMathChainlink for uint256;
-    address public owner;
     uint256 public usdEntryFee;
-    bool public isLotteryRunning;
     AggregatorV3Interface internal ethUsdPriceFeed;
-    modifier onlyOwner() {
-        require(msg.sender == owner);
-        _;
+    enum LOTTERY_STATE {
+        OPEN,
+        CLOSED,
+        CALCULATING
     }
 
+    LOTTERY_STATE public lottery_state;
+
     constructor(address _ethUsdPriceFeedAddress) public {
-        owner = msg.sender;
+        lottery_state = LOTTERY_STATE.CLOSED;
         usdEntryFee = 50 * (10**18);
         ethUsdPriceFeed = AggregatorV3Interface(_ethUsdPriceFeedAddress);
     }
@@ -24,12 +26,19 @@ contract Lottery {
     address payable[] public players;
 
     function enter() public payable {
+        require(
+            lottery_state == LOTTERY_STATE.OPEN,
+            "Lottery is not currently running please wait for admin to start!"
+        );
         // Set Minimum 50$
         uint256 feeAtEnterTime = getEntranceFee();
-        require(
-            msg.value > feeAtEnterTime,
-            "Fee is not large enought to take part in lottery!"
-        );
+
+        require(msg.value > feeAtEnterTime, "Fee too small!");
+        //founderror
+        //require(
+        //    msg.value > feeAtEnterTime,
+        //    "Fee is not large enought to take part in lottery!"
+        //);
         //Sends back change
         msg.sender.send(msg.value - feeAtEnterTime);
         players.push(msg.sender);
@@ -44,17 +53,19 @@ contract Lottery {
 
     function startLottery() public onlyOwner {
         require(
-            !isLotteryRunning,
+            lottery_state == LOTTERY_STATE.CLOSED &&
+                lottery_state != LOTTERY_STATE.CALCULATING,
             "Lottery is already running cannot start it."
         );
-        isLotteryRunning = true;
+        lottery_state = LOTTERY_STATE.OPEN;
     }
 
     function endLottery() public onlyOwner {
         require(
-            isLotteryRunning,
+            lottery_state != LOTTERY_STATE.CLOSED &&
+                lottery_state != LOTTERY_STATE.CALCULATING,
             "Lottery is not running. It must be started before it can be ended."
         );
-        isLotteryRunning = false;
+        lottery_state = LOTTERY_STATE.OPEN;
     }
 }
--- tests/test_lottery.py
+++ tests/test_lottery.py
@@ -1,11 +1,43 @@
-from brownie import Lottery
+from brownie import Lottery, accounts, web3, exceptions
+import pytest
 from scripts.helpful_scripts import get_account
 from scripts.deployLottery import deploy
+from web3 import Web3
+import pytest
 
 
 def test_get_entrance_fee():
+    # Arrange
     account = get_account()
     lotteryContract = deploy()
     entranceFee = lotteryContract.getEntranceFee()
     print(entranceFee)
-    assert entranceFee > 0
+    first_partecipant = accounts[1]
+    print(account.balance())
+    print(first_partecipant.balance())
+    # Expect exception on this
+    with pytest.raises(exceptions.VirtualMachineError):
+        lotteryContract.enter({"from": first_partecipant, "value": entranceFee - 10000})
+    with pytest.raises(exceptions.VirtualMachineError):
+        lotteryContract.enter({"from": first_partecipant, "value": entranceFee + 10000})
+    with pytest.raises(exceptions.VirtualMachineError):
+        lotteryContract.startLottery({"from": first_partecipant})
+    with pytest.raises(exceptions.VirtualMachineError):
+        lotteryContract.endLottery({"from": first_partecipant})
+    with pytest.raises(exceptions.VirtualMachineError):
+        lotteryContract.endLottery({"from": account})
+
+    lotteryContract.startLottery({"from": account})
+
+    with pytest.raises(exceptions.VirtualMachineError):
+        lotteryContract.enter({"from": first_partecipant, "value": entranceFee - 10000})
+
+    lotteryContract.enter({"from": first_partecipant, "value": entranceFee + 10000})
+    lotteryContract.enter({"from": accounts[2], "value": entranceFee * 100})
+    lotteryContract.enter({"from": accounts[3], "value": entranceFee * 100})
+
+    assert lotteryContract.balance() == 3 * entranceFee
+
+    print(lotteryContract.balance())
+    # assert entranceFee > web3.toWei(0.018, "ether")
+    # assert entranceFee < web3.toWei(0.022, "ether")
